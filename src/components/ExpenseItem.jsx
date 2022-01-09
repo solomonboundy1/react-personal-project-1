08b6588e41cb85a42ba97fddf12c32af7463726f
solomonboundy1 <solomonboundy2017@gmail.com>
2022-01-09 04:09:04 +0000

    made more readable

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,4 +1,5 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   //   const expenseDate = new Date(2022, 0, 5);
@@ -7,7 +8,7 @@ const ExpenseItem = (props) => {
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
