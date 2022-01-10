15749985998df2c392a9d90b1c4c8801ad1f4f58
solomonboundy1 <solomonboundy2017@gmail.com>
2022-01-10 14:11:33 +0000

    changes to layout

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-UK", { month: "long" });
   const day = props.date.toLocaleString("en-UK", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-day__day">{day}</div>
+      <div className="expense-day__month">{month}</div>
+      <div className="expense-day__year">{year}</div>
     </div>
   );
 };
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,20 +1,16 @@
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => {
-  //   const expenseDate = new Date(2022, 0, 5);
-  //   const expenseTitle = "Car Insurance";
-  //   const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <ExpenseDate />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">£{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
-};
+}
 
 export default ExpenseItem;
